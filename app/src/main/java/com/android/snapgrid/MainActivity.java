3ee65a140366c276600ff5bac1bdd306fe209e69
Thuy Tang <tangxuanthuy11122002@gmail.com>
2024-03-24 06:22:48 +0700

    upload DetailPost Activity alpha

--- app/src/main/java/com/android/snapgrid/MainActivity.java
+++ app/src/main/java/com/android/snapgrid/MainActivity.java
@@ -2,7 +2,9 @@ package com.android.snapgrid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-} 
+
+    public void goToDetailPost(View view) {
+        Intent intent = new Intent(this, DetailPost.class);
+        startActivity(intent);
+    }
+}
