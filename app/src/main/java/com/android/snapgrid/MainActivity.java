98d4b60056ee9477bfbf62391360bf88b75ac048
Thuy Tang <tangxuanthuy11122002@gmail.com>
2024-03-27 15:38:47 +0700

    Update detail post

--- app/src/main/java/com/android/snapgrid/MainActivity.java
+++ app/src/main/java/com/android/snapgrid/MainActivity.java
@@ -23,4 +23,24 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, UserInformation.class);
         startActivity(intent);
     }
+
+    public void gotoNavigation(View view) {
+        Intent intent = new Intent(this, NavigationActivity.class);
+        startActivity(intent);
+    }
+
+    public void gotoSlider(View view) {
+        Intent intent = new Intent(this, DetailPost.class);
+        startActivity(intent);
+    }
+
+    public void gotoSignIn(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void gotoSignUp(View view) {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
 }
