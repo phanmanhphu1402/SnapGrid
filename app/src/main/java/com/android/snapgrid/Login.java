9c4cf791b53df2c4953a4ce9095b2097bd990a23
Phú Phan Mạnh <phanmanhphuhoanam@gmail.com>
2024-04-12 22:14:36 +0700

    google-services.json,...

--- app/src/main/java/com/android/snapgrid/Login.java
+++ app/src/main/java/com/android/snapgrid/Login.java
@@ -277,7 +277,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == RC_SIGN_IN){
+            if(requestCode == RC_SIGN_IN){
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
 
             try {
