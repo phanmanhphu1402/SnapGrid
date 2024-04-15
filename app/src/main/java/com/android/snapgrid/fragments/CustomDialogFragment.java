577a6cebd12b094d3f7fdde5d7783831876d271e
lequangtruong2207 <ngantruong21102002@gmail.com>
2024-04-16 01:24:04 +0700

    handle download image

--- app/src/main/java/com/android/snapgrid/fragments/CustomDialogFragment.java
+++ app/src/main/java/com/android/snapgrid/fragments/CustomDialogFragment.java
@@ -3,6 +3,10 @@ package com.android.snapgrid.fragments;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -10,6 +14,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,11 +23,21 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.android.snapgrid.PostAddingActivity;
 import com.android.snapgrid.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Objects;
+import java.util.UUID;
 
 public class CustomDialogFragment extends DialogFragment {
     Button btnEdit, btnCopyLink, btnDownload, btnClose;
@@ -48,7 +64,7 @@ public class CustomDialogFragment extends DialogFragment {
         btnClose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dialog.dismiss();
+                dialog.cancel();
             }
         });
         btnEdit = view.findViewById(R.id.btnEdit);
@@ -56,7 +72,7 @@ public class CustomDialogFragment extends DialogFragment {
         System.out.println("THUY GGG FFF");
         System.out.println(currentUser.getUid());
         System.out.println(idUser);
-        if(!currentUser.getUid().equals(idUser)){
+        if (!currentUser.getUid().equals(idUser)) {
             btnEdit.setVisibility(View.INVISIBLE);
         }
         btnEdit.setOnClickListener(new View.OnClickListener() {
@@ -82,7 +98,34 @@ public class CustomDialogFragment extends DialogFragment {
                 startActivity(intent);
             }
         });
+        Log.d(CustomDialogFragment.class.getName(), Objects.requireNonNull(bundle.getString("dataImage")) + "");
+        btnDownload = view.findViewById(R.id.btnDownload);
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String imageUrl = bundle.getString("dataImage");
+                Picasso picasso = Picasso.get();
+                picasso.setIndicatorsEnabled(true);
+                picasso.setLoggingEnabled(true);
+                picasso.load(imageUrl).into(new Target() {
+                    @Override
+                    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                        MediaStore.Images.Media.insertImage(requireContext().getContentResolver(), bitmap, bundle.getString("dataTitle"), bundle.getString("dataContent"));
+                        Toast.makeText(getContext(), "Download image is success!", Toast.LENGTH_SHORT).show();
+                    }
 
+                    @Override
+                    public void onBitmapFailed(Exception e, Drawable errorDrawable) {
+                        Toast.makeText(getContext(), "Download image is failed!", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPrepareLoad(Drawable placeHolderDrawable) {
+                        Toast.makeText(getContext(), "Downloading...", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
         return dialog;
     }
